update num_steps, which affects training a lot; fix the accleration computation bug; add options for traj_encoder and obs_encoder

PPO/PPO_continuous_sg.py
@@ -82,11 +82,20 @@ class PositionalEncoding(nn.Module):
 
 
 class Transfromer_Linear(nn.Module):
-    def __init__(self, input_size, hidden_size, num_transf_layers, num_linear_layers, output_size, batch_first=True, init_std=0.01) -> None:
+    def __init__(self, input_size, hidden_size, num_transf_layers, num_linear_layers, output_size, nhead=1, batch_first=True, init_std=0.01) -> None:
         super().__init__()
         self.positional_encoding = PositionalEncoding(input_size, batch_first=batch_first)
-        transformer_layer = nn.TransformerEncoderLayer(d_model=input_size, nhead=1, dim_feedforward=hidden_size, batch_first=batch_first)
-        self.transformer = nn.TransformerEncoder(transformer_layer, num_layers=num_transf_layers, norm=nn.LayerNorm(input_size))
+        transformer_layer = nn.TransformerEncoderLayer(
+            d_model=input_size, 
+            nhead=nhead, 
+            dim_feedforward=hidden_size, 
+            batch_first=batch_first
+        )
+        self.transformer = nn.TransformerEncoder(
+            transformer_layer, 
+            num_layers=num_transf_layers, 
+            norm=nn.LayerNorm(input_size)
+        )
         self.linear = MLP(
             input_size=input_size,
             hidden_size=hidden_size,
@@ -104,10 +113,12 @@ class Transfromer_Linear(nn.Module):
     
 
 class MLP(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size, num_layers, use_relu=False, init_std=1., auto_flatten=False):
+    def __init__(self, input_size, hidden_size, output_size, num_layers, use_relu=False, init_std=1., auto_flatten=False, flatten_start_dim=1):
         super().__init__()
         self.activation = nn.ReLU() if use_relu else nn.Tanh()
-        self.mlp = nn.Sequential() if not auto_flatten else nn.Sequential(AutoFlatten())
+        self.mlp = nn.Sequential() \
+                   if not auto_flatten \
+                   else nn.Sequential(AutoFlatten(start_dim=flatten_start_dim))
         for i in range(num_layers-1):
             input_shape = input_size if i==0 else hidden_size
             self.mlp.append(layer_init(nn.Linear(input_shape, hidden_size)))
@@ -132,7 +143,6 @@ class Agent(nn.Module):
         super().__init__()
         self.envs = envs
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.use_transformer = envs.args.use_transformer
         self.activation = nn.Tanh() if not envs.args.use_relu else nn.ReLU()
         self.deterministic = envs.args.deterministic
         self.hidden_size = envs.args.hidden_size
@@ -149,7 +159,7 @@ class Agent(nn.Module):
         self.pc_extractor.load_checkpoint(ckpt_path="PointNet_Model/checkpoints/best_model.pth", evaluate=True, map_location=self.device)
 
         # Trajectory history encoder and Large sequence observation encoder
-        if self.use_transformer:
+        if envs.args.use_tf_traj_encoder:
             self.traj_hist_encoder = Transfromer_Linear(
                 input_size=envs.traj_history_shape[1], 
                 hidden_size=self.hidden_size, 
@@ -159,16 +169,6 @@ class Agent(nn.Module):
                 batch_first=True, 
                 init_std=1.0
             ).to(self.device)
-            self.seq_obs_encoder = Transfromer_Linear(
-                input_size=envs.post_act_hist_qr_ft_shape[2], 
-                hidden_size=self.hidden_size, 
-                num_transf_layers=self.seq_obs_encoder_num_transf,
-                num_linear_layers=self.seq_obs_encoder_num_linear, 
-                output_size=envs.seq_info_ft_dim, 
-                batch_first=True, 
-                init_std=1.0
-            ).to(self.device)
-            
         else:
             self.traj_hist_encoder = MLP(
                 input_size=np.prod(envs.traj_history_shape), 
@@ -178,6 +178,18 @@ class Agent(nn.Module):
                 use_relu=envs.args.use_relu,
                 init_std=1.0, auto_flatten=True
             ).to(self.device)
+        
+        if envs.args.use_tf_seq_obs_encoder:
+            self.seq_obs_encoder = Transfromer_Linear(
+                input_size=envs.post_act_hist_qr_ft_shape[2], 
+                hidden_size=self.hidden_size, 
+                num_transf_layers=self.seq_obs_encoder_num_transf,
+                num_linear_layers=self.seq_obs_encoder_num_linear, 
+                output_size=envs.seq_info_ft_dim, 
+                batch_first=True, 
+                init_std=1.0
+            ).to(self.device)
+        else:
             self.seq_obs_encoder = MLP(
                 input_size=np.prod(envs.post_act_hist_qr_ft_shape[1:]), 
                 hidden_size=self.hidden_size, 
@@ -214,6 +226,8 @@ class Agent(nn.Module):
     def seq_obs_ft_extract(self, seq_obs):
         """
         Preprocess the sequence observation and extract the features
+        input seq_obs: (Env, Seq, Dim1)
+        output seq_obs_ft: (Env, Dim2)
         """
         if self.envs.args.use_traj_encoder:
             qr_region_hist = seq_obs[:, :, self.envs.qr_region_slice]
@@ -226,7 +240,7 @@ class Agent(nn.Module):
         if self.envs.args.use_seq_obs_encoder:
             seq_obs = self.seq_obs_encoder(seq_obs)
         else:
-            # Flatten the sequence observation to (Env, Seq*Dim)
+            # Flatten the sequence observation from (Env, Seq, Dim) to (Env, Seq*Dim)
             seq_obs = torch.flatten(seq_obs, start_dim=1)
         return seq_obs
     

RoboSensai_bullet.py
@@ -205,6 +205,9 @@ class RoboSensaiBullet:
         num_queriable_scenes = len(self.queriable_obj_names) + len(self.fixed_scene_name_data.keys()) if not self.args.fixed_scene_only else len(self.fixed_scene_name_data.keys())
         assert num_queriable_scenes >= self.args.max_num_qr_scenes, f"Only {num_queriable_scenes} scenes are loaded, but we need {self.args.max_num_qr_scenes} scenes!"
         assert len(self.obj_name_data) >= self.args.max_num_placing_objs, f"Only {len(self.obj_name_data)} objects are loaded, but we need {self.args.max_num_placing_objs} objects!"
+        
+        verbose_info = f"Loaded {len(self.obj_name_data)} objects from {self.args.object_pool_folder} | {self.args.max_num_placing_objs} objects will be placed."
+        print("*"*len(verbose_info)); print(verbose_info); print("*"*len(verbose_info))
 
     
     def load_world(self):
@@ -397,6 +400,7 @@ class RoboSensaiBullet:
             pu.set_pose(self.selected_obj_id, (pose_xyz, pose_quat), client_id=self.client_id)
             self.traj_history = [[0.]* (7 + 6)] * self.args.max_traj_history_len  # obj_pos dimension + obj_vel dimension
             self.accm_vel_reward = 0.
+            self.prev_obj_vel = np.array([0.]*6, dtype=self.numpy_dtype)
             self.his_steps = 0
             self.continue_stable_steps = 0
             self.info['stepping'] = 0.
@@ -404,16 +408,16 @@ class RoboSensaiBullet:
             self.info['pc_change_indicator'] = 0.
         
         # stepping == 0 means previous action is still running, we need to wait until the object is stable
+        # TODO: Stable check is not only based on the placed object, but the maximum vel in the whole scene!!
         # In-pysical step
         if self.info['stepping'] == 0.:
-            self.prev_obj_vel = np.array([0.]*6, dtype=self.numpy_dtype)
             for _ in range(ceil(self.args.max_traj_history_len/self.args.step_divider)):
                 self.simstep(1/240)
                 self.blender_record()
 
                 obj_pos, obj_quat = pu.get_body_pose(self.selected_obj_id, client_id=self.client_id)
                 obj_vel = pu.getObjVelocity(self.selected_obj_id, to_array=True, client_id=self.client_id)
-                # Update the trajectory history [0, 0, 0, ..., T0, T1..., Tn]
+                # Update the trajectory history [0, 0, 0, ..., T0, T1..., Tn]; Left Shift
                 self.traj_history.pop(0)
                 self.traj_history.append(obj_pos + obj_quat + obj_vel.tolist())
                 # Accumulate velocity reward
@@ -526,6 +530,7 @@ class RoboSensaiBullet:
         # Update the sequence observation | pop the first observation and append the last observation
         his_traj = self.to_numpy(self.traj_history).flatten()
         cur_seq_obs = np.concatenate([self.selected_qr_region, self.last_raw_action, his_traj])
+        # Left shift the sequence observation
         self.last_seq_obs = np.concatenate([self.last_seq_obs[1:, :], np.expand_dims(cur_seq_obs, axis=0)])
         
         return self.last_seq_obs

ppo_sg_asyn.py
@@ -44,7 +44,7 @@ def parse_args():
     parser.add_argument('--random_select_scene_pool', type=lambda x: bool(strtobool(x)), default=False, nargs='?', const=True, help='Draw contact force direction')
     parser.add_argument('--random_select_placing', type=lambda x: bool(strtobool(x)), default=True, nargs='?', const=True, help='Draw contact force direction')
     parser.add_argument('--fixed_scene_only', type=lambda x: bool(strtobool(x)), default=True, nargs='?', const=True, help='Draw contact force direction')
-    parser.add_argument('--fixed_qr_region', type=lambda x: bool(strtobool(x)), default=True, nargs='?', const=True, help='Draw contact force direction')
+    parser.add_argument('--fixed_qr_region', type=lambda x: bool(strtobool(x)), default=False, nargs='?', const=True, help='Draw contact force direction')
     parser.add_argument('--num_episode_to_replace_pool', type=int, default=np.inf)
     parser.add_argument('--max_num_urdf_points', type=int, default=2048)
     parser.add_argument('--max_num_scene_points', type=int, default=10240)
@@ -74,12 +74,13 @@ def parse_args():
     # Algorithm specific arguments
     parser.add_argument('--env_name', default="RoboSensai_SG", help='Wandb config name')
     parser.add_argument("--use_lstm", type=lambda x: bool(strtobool(x)), default=False, nargs="?", const=True, help="Toggles whether or not to use LSTM version of meta-controller.")
-    parser.add_argument("--use_transformer", type=lambda x: bool(strtobool(x)), default=False, nargs="?", const=True, help="Toggles whether or not to use Transformer version of meta-controller.")
     parser.add_argument("--use_traj_encoder", type=lambda x: bool(strtobool(x)), default=False, nargs="?", const=True, help="Toggles whether or not to use Transformer version of meta-controller.")
     parser.add_argument("--use_seq_obs_encoder", type=lambda x: bool(strtobool(x)), default=False, nargs="?", const=True, help="Toggles whether or not to use Transformer version of meta-controller.")
+    parser.add_argument("--use_tf_traj_encoder", type=lambda x: bool(strtobool(x)), default=False, nargs="?", const=True, help="Toggles whether or not to use Transformer version of meta-controller.")
+    parser.add_argument("--use_tf_seq_obs_encoder", type=lambda x: bool(strtobool(x)), default=False, nargs="?", const=True, help="Toggles whether or not to use Transformer version of meta-controller.")
     parser.add_argument("--total_timesteps", type=int, default=int(1e9), help="total timesteps of the experiments")
     parser.add_argument("--num_envs", type=int, default=10, help="the number of parallel game environments")
-    parser.add_argument("--num-steps", type=int, default=128, help="the number of steps to run in each environment per policy rollout per object")
+    parser.add_argument("--num-steps", type=int, default=256, help="the number of steps to run in each environment per policy rollout per object")
     parser.add_argument("--pc_batchsize", type=int, default=None, help="the number of steps to run in each environment per policy rollout per object")
     parser.add_argument("--use_relu", type=lambda x: bool(strtobool(x)), default=True, nargs="?", const=True, help="Use Relu or tanh.")
     parser.add_argument("--anneal-lr", type=lambda x: bool(strtobool(x)), default=True, nargs="?", const=True, help="Toggle learning rate annealing for policy and value networks")
@@ -116,7 +117,8 @@ def parse_args():
     args = parser.parse_args()
 
     # Training required attributes
-    args.num_steps = args.num_steps * args.max_num_placing_objs # make sure the num_steps is 5 times larger than agent traj length
+    assert args.num_steps > args.max_num_placing_objs * args.max_trials * 5, \
+        f"num_steps now {args.num_steps} should be 5 times larger than agent traj length {args.max_num_placing_objs * args.max_trials * 5} to avoid overfitting"
     args.batch_size = int(args.num_envs * args.num_steps)
     args.num_minibatches = ceil(args.batch_size // args.minibatch_size)
     args.pc_batchsize = args.pc_batchsize if args.pc_batchsize is not None else args.num_envs
@@ -132,10 +134,11 @@ def parse_args():
     # Uniformalize training name
     additional = ''
     ###--- suffix for final name ---###
-    if args.use_lstm: additional += '_LSTM'
-    elif args.use_transformer: additional += '_Transformer'
-    else: additional += '_FC'
-    if args.checkpoint is not None: additional += '_FT'
+    if args.use_traj_encoder: 
+        additional += '_TrajEncoderTF' if args.use_tf_traj_encoder else '_TrajEncoderFC'
+    if args.use_seq_obs_encoder:
+        additional += '_SeqObsEncoderTF' if args.use_tf_seq_obs_encoder else '_SeqObsEncoderFC'
+    if args.checkpoint is not None: additional += '_FineTune'
     if args.use_relu: additional += '_Relu'
     else: additional += '_Tanh'
     additional += '_Rand'
@@ -156,6 +159,7 @@ def parse_args():
     if args.reward_pobj > 0: additional += f'_rewardPobj{args.reward_pobj}'
     if args.penalty > 0: additional += f'_ori{args.penalty}'
     additional += f'_seq{args.sequence_len}'
+    additional += f'_step{args.num_steps}'
 
     args.timer = '_' + '_'.join(str(datetime.datetime.now())[5:16].split())  # a time name file
 