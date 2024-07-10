98fbe15e9a597b5a2390c08a93d0a5bfafe6a304
Ys-Jia <yj2677@columbia.edu>
2024-07-10 19:08:04 +0800

    uniformalize the checking condition

--- RoboSensai_bullet.py
+++ RoboSensai_bullet.py
@@ -535,9 +535,8 @@ class RoboSensaiBullet:
                 self.continue_stable_steps += 1
                 if self.continue_stable_steps >= self.args.min_continue_stable_steps: 
                     break
-            # TODO: Uncomment this part.
-            # else:
-            #     self.continue_stable_steps = 0
+            else:
+                self.continue_stable_steps = 0
 
             self.prev_obj_vel = obj_vel
 
